aacdcfbe71ad4059760708929ad523964896c7fb
Horacio Moreno <hmorenom@umich.edu>
2021-06-14 16:09:42 -0400

    did stuff

---
+++ OgModel/param_vis_velocity.py
@@ -0,0 +1,351 @@
+#!/opt/anaconda3/bin/python
+import os
+from datetime import datetime
+from idlpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+import hissw
+import math
+
+ssw = hissw.Environment(
+    ssw_packages=['packages/chianti/'], ssw_paths=['chianti'])
+
+#parameters# 
+
+def cutAndTransformToArray(l, ind):
+    l = l[ind:]
+    l = np.array(l)
+    return l
+
+
+def parametrize(parameters):
+
+    overflow = False
+    c = parameters
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel'  \n")
+    IDL.run("restore,'fast_wind.save'\n", stdout=True)
+
+    height = IDL.HEIGHT
+    bfield = IDL.BFIELD
+    alfven = IDL.ALFVEN
+
+    original_mass = IDL.mass
+    original_temp = IDL.temp
+    original_vel = IDL.VELOCITY 
+
+    print(height.shape)
+    print(original_vel.shape)
+
+    mass = []
+
+    temp = []
+
+    velocity = []
+
+    x_min = c[8]/(1.00925 ** c[9]) + c[10]/(1.00925 ** c[11])
+
+    for r in height:
+        
+        m = (c[0]/(r**c[1])) + (c[5]/(r**c[6]))
+        
+        T = c[2]*(((c[3]*r)**c[7])/(r-c[4]))*math.exp(-(c[3]/(r-c[4]))**3)
+        
+        x = c[8]/(r ** c[9]) + c[10]/(r ** c[11])
+        v = x_min - x
+        mass.append(m)
+        temp.append(T)
+        velocity.append(v)
+
+    ind_20k = 0
+
+    ind_vel = 0
+
+    print(velocity[0])
+
+    for index, elem in enumerate(temp):
+        if elem > 2e4:
+            ind_20k = index
+            break
+
+    for index, elem in enumerate(velocity):
+        if elem > 1:
+            ind_vel = index
+            break
+
+    index = max(ind_20k, ind_vel) 
+
+    index = 0
+
+    print(index)
+
+    height = cutAndTransformToArray(height, index)
+    mass = cutAndTransformToArray(mass, index)
+    temp = cutAndTransformToArray(temp, index)
+    velocity = cutAndTransformToArray(velocity, index)
+    bfield = cutAndTransformToArray(bfield, index)
+    alfven = cutAndTransformToArray(alfven, index)
+
+    original_temp = cutAndTransformToArray(original_temp, index)
+    original_mass = cutAndTransformToArray(original_mass, index)
+    original_vel = cutAndTransformToArray(original_vel, index)
+    
+    IDL.mass = mass
+    IDL.temp = temp
+    IDL.height = height
+    IDL.velocity = velocity
+    IDL.bfield = bfield
+    IDL.alfven = alfven
+    
+    #IDL.run("ratio = proton_dens(alog10(temp), /hydrogen, abund_file=\'/usr/local/ssw/packages/chianti/dbase/abundance/sun_photospheric_2011_caffau.abund\', ioneq_file = \'/usr/local/ssw/packages/chianti/dbase/ioneq/chianti.ioneq\')\n")
+
+    IDL.run("save, units, height, mass, velocity, temp, bfield, alfven, filename= 'paramvis_fast_wind.save', /verb ")
+    #ratio = IDL.RATIO
+
+
+    return np.array([height, original_mass, original_temp, original_vel, mass, temp, velocity]), index
+    #IDL.run("save, units, height, mass, velocity, temp, bfield, alfven, filename= 'param_fast_wind.save', /verb ")
+
+    #logging.info('Saved param_fast_wind.save file.')
+
+
+def get_density():
+    
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/density'  \n")
+    IDL.run("restore,'density_original.save'\n", stdout=True)
+
+    original_density = IDL.DENSITY
+
+    original_density = np.array(original_density)
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/density'  \n")
+    IDL.run("restore,'density_parameters.save'\n", stdout=True)
+
+    parameters_density = IDL.DENSITY
+    parameters_density = np.array(parameters_density)
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/density'  \n")
+    IDL.run("restore,'density_starting_params.save'\n", stdout=True)
+
+    start_parameters_density = IDL.DENSITY
+    start_parameters_density = np.array(parameters_density)
+
+    return np.array([original_density, parameters_density, start_parameters_density])
+
+def get_carbon_ion():
+
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/FastWindData'  \n")
+    IDL.run("restore,'fast_wind_measurements.save', /v\n", stdout=True)
+    IDL.run("measured_ion = carbon")
+
+    measured_ion = IDL.MEASURED_ION
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/temp_vis'  \n")
+    IDL.run("restore,'pred_c.save'\n", stdout=True)
+    IDL.run("predicted_ion = ioneq_evol(*,-1)")
+
+    predicted_ion = IDL.PREDICTED_ION
+
+    return np.array([measured_ion, predicted_ion])
+
+def get_oxygen_ion():
+
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/FastWindData'  \n")
+    IDL.run("restore,'fast_wind_measurements.save', /v\n", stdout=True)
+    IDL.run("measured_ion = oxygen")
+
+    measured_ion = IDL.MEASURED_ION
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/temp_vis'  \n")
+    IDL.run("restore,'pred_o.save'\n", stdout=True)
+    IDL.run("predicted_ion = ioneq_evol(*,-1)")
+
+    predicted_ion = IDL.PREDICTED_ION
+
+    return np.array([measured_ion, predicted_ion])
+
+def get_iron_ion():
+
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/FastWindData'  \n")
+    IDL.run("restore,'fast_wind_measurements.save', /v\n", stdout=True)
+    IDL.run("measured_ion = iron")
+
+    measured_ion = IDL.MEASURED_ION
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel/temp_vis'  \n")
+    IDL.run("restore,'pred_fe.save'\n", stdout=True)
+    IDL.run("predicted_ion = ioneq_evol(*,-1)")
+
+    predicted_ion = IDL.PREDICTED_ION
+
+    return np.array([measured_ion, predicted_ion])
+    
+def plot_temp(values_end,  values_start):
+
+    fig_dens, (ax1) = plt.subplots(1, 1, figsize=(10, 10))
+    ax1.plot(values_end[0], values_end[2], color='green', label='Theoretical Temperature')
+    ax1.plot(values_end[0], values_end[5], color='blue',
+             label='Final Parametrized Temperature')
+    ax1.plot(values_start[0], values_start[5], 'g--', color='red',
+             label='Initial Parametrized Temperature')
+    ax1.set_xlabel('Distance from the Sun [solar radii] (log scale)')
+    ax1.set_ylabel('Temperature [K] (log scale)')
+    ax1.set(title='Temperature vs. Distance from the Sun')
+    ax1.grid()
+    #ax2.plot(values[0], values[1], color='green', label='Original Mass')
+    #ax2.plot(values[0], values[3], color='blue',
+    #         label='First Parametrized Mass')
+    #ax2.set_xlabel('Distance (log(solar radii))')
+    #ax2.set_ylabel('Mass')
+    #ax2.set(title='Mass')
+    ax1.legend()
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+
+    #ax2.set_yscale('log')
+    #ax2.set_xscale('log')
+
+def plot_vel(values_end, values_start):
+
+    fig_dens, (ax1) = plt.subplots(1, 1, figsize=(10, 10))
+    #ax1.plot(values_end[0], values_end[3], color='green', label='Theoretical Velocity')
+    ax1.plot(values_end[0], values_end[6], color='blue',
+             label='Final Parametrized Velocity')
+    ax1.plot(values_start[0], values_start[6], 'g--', color='red',
+             label='Initial Parametrized Velocity')
+    ax1.hlines(y=c[-2], xmin = values_end[0][0], xmax = values_end[0][-1], color='r', linestyle='-')
+    ax1.set_xlabel('Distance from the Sun [solar radii] (log scale)')
+    ax1.set_ylabel('Velocity (log scale)')
+    ax1.set(title='Velocity vs. Distance from the Sun')
+    ax1.grid()
+    #ax2.plot(values[0], values[1], color='green', label='Original Mass')
+    #ax2.plot(values[0], values[3], color='blue',
+    #         label='First Parametrized Mass')
+    #ax2.set_xlabel('Distance (log(solar radii))')
+    #ax2.set_ylabel('Mass')
+    #ax2.set(title='Mass')
+    ax1.legend()
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+
+    #ax2.set_yscale('log')
+    #ax2.set_xscale('log')
+
+
+def plot_densities(height, dens):
+
+    fig, (ax1) = plt.subplots(1, 1, figsize=(10, 10))
+    ax1.plot(height, dens[0][705:], color='green', label='Theoretical Electron Density')
+    ax1.plot(height, dens[1][1:], color='blue',
+             label='Final Parametrized Electron Density')
+    ax1.plot(height, dens[2][1:], 'g--', color='red',
+             label='Starting Parametrized Electron Density')
+    ax1.set_xlabel('Distance from the Sun[solar radii] (log scale) ')
+    ax1.set_ylabel('Electron Density (log scale)')
+    ax1.set(title='Electron Density vs. Distance from the Sun')
+    ax1.grid()	
+
+    #ax2.plot(values[0], values[1], color='green', label='Original Mass')
+    #ax2.plot(values[0], values[3], color='blue',
+    #         label='First Parametrized Mass')
+    #ax2.set_xlabel('Distance (log(solar radii))')
+    #ax2.set_ylabel('Mass')
+    #ax2.set(title='Mass')
+
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+    ax1.legend()
+
+    #ax2.set_yscale('log')
+    #ax2.set_xscale('log')
+
+def plot_ion(ions):
+    
+    fig_ion, (ax1) = plt.subplots(1,1, figsize=(26,15))
+    X = np.arange(7)
+    ax1.bar(X - 0.20, ions[0], color = 'b', width = 0.4, label='Measured Fractions')
+    ax1.bar(X + 0.20, ions[1], color = 'g', width = 0.4, label='Predicted Fractions')
+    
+    ax1.set_xlabel('Ionization Stages [+e]')
+    ax1.set_ylabel('Fraction of Total Number of C Atoms')	
+    ax1.set(title='Fraction of Measured and Predicted Carbon Ions')
+    ax1.legend()
+
+def plot_ions(ions_c, ions_o, ions_fe):
+    
+    fig_ion, (ax1,ax2,ax3) = plt.subplots(1,3, figsize=(20,8))
+    ions_c_cut = [ions_c[0,3:7], ions_c[1,3:7]]
+    X = np.arange(3,7)
+    ax1.bar(X - 0.20, ions_c_cut[0], color = 'b', width = 0.4, label='Measured Fractions')
+    ax1.bar(X + 0.20, ions_c_cut[1], color = 'g', width = 0.4, label='Predicted Fractions')
+    
+    ax1.set_xlabel('Ionization Stages [+e]')
+    ax1.set_ylabel('Fraction of Total Number of C Atoms')	
+    ax1.set(title='Fraction of Measured and Predicted Carbon Ions')
+    ax1.legend()
+    ax1.set_xticks(np.arange(min(X), max(X)+1, 1.0))    
+
+    Z = np.arange(4,9)
+    ions_o_cut = [ions_o[0,4:9], ions_o[1,4:9]]
+    ax2.bar(Z - 0.20, ions_o_cut[0], color = 'b', width = 0.4, label='Measured Fractions')
+    ax2.bar(Z + 0.20, ions_o_cut[1], color = 'g', width = 0.4, label='Predicted Fractions')
+    
+    ax2.set_xlabel('Ionization Stages [+e]')
+    ax2.set_ylabel('Fraction of Total Number of O Atoms')	
+    ax2.set(title='Fraction of Measured and Predicted Oxygen Ions')
+    ax2.legend()
+    ax2.set_xticks(Z)
+
+    ions_fe_cut = [ions_fe[0,7:17], ions_fe[1,7:17]]
+    Y =  np.arange(7,17)
+    ax3.bar(Y - 0.20, ions_fe_cut[0], color = 'b', width = 0.4, label='Measured Fractions')
+    ax3.bar(Y + 0.20, ions_fe_cut[1], color = 'g', width = 0.4, label='Predicted Fractions')
+    
+    ax3.set_xlabel('Ionization Stages [+e]')
+    ax3.set_ylabel('Fraction of Total Number of Fe Atoms')	
+    ax3.set(title='Fraction of Measured and Predicted Iron Ions')
+    ax3.legend()
+    ax3.set_xticks(Y)
+
+
+c = [2.36196e-17, 3.63, 2000000.0, 0.4, 0.75, 2.42e-15, 21.87, 0.7128]
+
+c =[4.2000000000000005e-17, 2.3213428125, 2000000.0, 0.361, 0.748125, 2e-15, 30, 0.8, 2000, 600, 687.66667, 0.4]
+
+starting_c = [4e-17, 3, 2000000.0, 0.4, 0.75, 2e-15, 30, 0.8, 2000, 600, 687.66667, 0.4]
+
+
+
+values, index = parametrize(c)
+
+#ssw.run('run_wind_vis')
+
+start_vals, start_index = parametrize(starting_c)
+
+plot_temp(values, start_vals)
+
+plot_vel(values, start_vals)
+
+#densities = get_density()
+
+#print(densities[0].shape)
+
+#densities[0] = densities[0][index:]
+ 
+#densities[1] = densities[1, index:]
+
+#plot_densities(values[0], densities)
+
+#c_ions = get_carbon_ion()
+#o_ions = get_oxygen_ion()
+#fe_ions = get_iron_ion()
+
+
+#print(fe_ions)
+
+#plot_ions(c_ions, o_ions, fe_ions)
+
+plt.show()
---
+++ model_eval_velocity.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python3
+import os
+from datetime import datetime
+from idlpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+import hissw
+import math
+import logging
+
+ssw = hissw.Environment(
+    ssw_packages=['packages/chianti/'], ssw_paths=['chianti'])
+
+
+def createDirectory(path):
+
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y $H:$M:$S")
+    path = path + "/model_{}".format(now)
+    os.mkdir(path)
+    return path
+
+
+def get_observations():
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/FastWindData' \n")
+    IDL.run("restore,'fast_wind_measurements.save'\n", stdout=True)
+    c_obs = IDL.carbon
+    n_obs = IDL.nitrogen
+    o_obs = IDL.oxygen
+    ne_obs = IDL.neon
+    mg_obs = IDL.magnesium
+    si_obs = IDL.silicon
+    s_obs = IDL.sulphur
+    fe_obs = IDL.iron
+
+    observations = [c_obs, n_obs, o_obs, ne_obs, mg_obs, si_obs, s_obs, fe_obs]
+
+    return observations
+
+
+# takes in a list and an index and returns a numpy array of all elements in the list after the index
+def cutAndTransformToArray(l, index):
+
+    l = l[index:]
+    l = np.array(l)
+    return(l)
+
+
+def parametrize(parameters):
+
+    overflow = False
+    c = parameters
+
+    IDL.run("cd, '/home/hmorenom/SSW_Files/OgModel'  \n")
+    IDL.run("restore,'fast_wind.save'\n", stdout=True)
+
+    height = IDL.HEIGHT
+    bfield = IDL.BFIELD
+    alfven = IDL.ALFVEN
+
+    mass = []
+
+    temp = []
+
+    velocity = []
+
+    x_min = c[8]/(1.00925 ** c[9]) + c[10]/(1.00925 ** c[11])
+
+    for r in height:
+        try:
+            m = (c[0]/(r**c[1])) + (c[5]/(r**c[6]))
+        except OverflowError:
+            logging.info(
+                'Overflow Error in mass calculation, skipping set of parameters.')
+            overflow = True
+        try:
+            T = c[2]*(((c[3]*r)**c[7])/(r-c[4]))*math.exp(-(c[3]/(r-c[4]))**3)
+        except OverflowError:
+            logging.info(
+                'Overflow Error in temperature calculation, skipping set of parameters.')
+            overflow = True
+        try:
+            x = c[8]/(r ** c[9]) + c[10]/(r ** c[11])
+            v = x_min - x
+        except OverflowError:
+            logging.info(
+                'Overflow Error in velocity calculation, skipping set of parameters.')
+            overflow = True
+        mass.append(m)
+        temp.append(T)
+        velocity.append(v)
+        
+
+    ind_20k = 0
+
+    ind_vel = 0
+
+    for index, elem in enumerate(temp):
+        if elem > 2e4:
+            ind_20k = index
+            break
+
+    for index, elem in enumerate(velocity):
+        if elem > 1:
+            ind_vel = index
+            break
+
+    index = max(ind_20k, ind_vel)
+
+    height = cutAndTransformToArray(height, index)
+    mass = cutAndTransformToArray(mass, index)
+    temp = cutAndTransformToArray(temp, index)
+    velocity = cutAndTransformToArray(velocity, index)
+    bfield = cutAndTransformToArray(bfield, index)
+    alfven = cutAndTransformToArray(alfven, index)
+
+    IDL.mass = mass
+    IDL.temp = temp
+    IDL.height = height
+    IDL.velocity = velocity
+    IDL.bfield = bfield
+    IDL.alfven = alfven
+
+    IDL.run("save, units, height, mass, velocity, temp, bfield, alfven, filename= 'param_fast_wind.save', /verb ")
+
+    logging.info('Saved param_fast_wind.save file.')
+
+    return overflow
+
+
+# takes array with filenames and returns array that includes all predictions
+def getPredictions(files):
+
+    path = '/home/hmorenom/SSW_Files/OgModel/temp/'
+
+    IDL.run("cd, '" + path + "'  \n")
+
+    predictions = []
+
+    for x in filenames:
+        IDL.run("restore, '"+x + "'\n")
+        IDL.run("pred = ioneq_evol(*,-1) \n")
+        pred = IDL.pred
+        predictions.append(pred)
+
+    return np.array(predictions)
+
+
+# takes array that includes obbservations and array of filenames
+def chiSquared(observations, predictions):
+
+    chi_2_errors = []
+
+    for x in range(len(predictions)):
+        error = 0
+        for i in range(len(predictions[x])):
+            if observations[x][i] != 0:
+                error = error + \
+                    ((predictions[x][i]-observations[x][i])
+                     ** 2/observations[x][i])
+            else:
+                if predictions[x][i] > 1e-3:
+                    logging.info(
+                        "Element {}, ionization stage {} has predted value > 10^3".format(x, i))
+                    error = error + ((predictions[x][i]-1e-3)**2/1e-3)
+        chi_2_errors.append(error)
+
+    return chi_2_errors
+
+
+# takes parameter values and observation ioneq values, returns an array containing (params, chi2)
+def run_iteration(params, obs, filenames):
+
+    overflow = parametrize(params)
+    if overflow != True:
+        try:
+            logging.info('Running run_wind program')
+            ssw.run('run_wind')
+        except Exception as e:
+            print('Error when running run_wind, error: {}'.format(e))
+            logging.info("Failed to run run_wind program. Error: {}".format(e))
+        predictions = getPredictions(filenames)
+
+        chi_2_val = chiSquared(obs, predictions)
+
+        return [params, chi_2_val, overflow]
+    else:
+        return [0, 0, overflow]
+
+
+def randomize_parameters(parameters, factor=0.1):  # randomizes a random parameter by 10%
+
+    params = parameters[:]
+    index = 2
+
+    while (index == 2 or index == 10):
+        index = np.random.randint(len(params))
+
+    sign = np.random.choice([-1, 1])
+
+    params[index] += sign*factor*params[index]
+
+    return params
+
+
+def apply_Metropolis(current_iteration, new_iteration):
+
+    u = np.random.rand()  # generate random float between 0 and 1
+    A = min(
+        1, np.sum(np.array(new_iteration[1])) / np.sum(np.array(current_iteration)[1]))
+
+    if (u > A):
+        return new_iteration
+    else:
+        return current_iteration
+
+
+def run_MCMC(obs, initial_params, iterations, filenames, log_directory, factor=0.1):
+
+    logging.info('Runing MCMC for {} iterations. Initial parameters: {}'.format(
+        iterations, initial_params))
+
+    update_progress(0)
+
+    best_iteration = run_iteration(initial_params, obs, filenames)
+    old_params = initial_params[:]
+
+    chi2_values = []
+
+    for x in range(iterations):
+
+        logging.info(
+            '-------------------Begin iteration {} / {}.----------------------'.format(x + 1, iterations))
+
+        new_params = randomize_parameters(old_params, factor=factor)
+        logging.info('New randomized parameters: {}'.format(new_params))
+
+        new_iteration = run_iteration(new_params, obs, filenames)
+
+        # Check if there was not an overflow error in the calculation
+        if new_iteration[2] != True:
+
+            logging.info('Running Metropolis algorithm...')
+
+            best_iteration = apply_Metropolis(best_iteration, new_iteration)
+
+            logging.info('Ran Metropolis algorithm. Best set of parameters is {} with chi2 error of {}.'.format(
+                best_iteration[0], np.sum(np.array(best_iteration[1]))))
+
+            old_params = best_iteration[0]
+
+        else:
+
+            logging.info('Overflow error detected, skipping this value')
+
+        chi2_values.append(np.sum(np.array(best_iteration[1])))
+
+        # if we have found a better set of parameters
+        if best_iteration[0] == new_iteration[0]:
+
+            save_chi2(iterations, chi2_values, log_directory)
+
+        update_progress((x+1)/iterations)
+
+    return best_iteration, chi2_values
+
+
+def plot_chi2(iterations, chi2_vals, filename, display=False):
+
+    plt.plot(np.arange(1, iterations + 1), chi2_vals, color='r')
+    plt.grid()
+    plt.title('Error vs. Number of Iterations')
+    plt.xlabel('Number of Iterations', fontsize=14)
+    plt.ylabel(r'Sum of $\chi^2$ Values For All Elements')
+
+    plt.savefig(filename)
+
+    logging.info('Saved error plot.')
+
+    if display:
+        plt.show()
+
+
+def save_chi2(iterations, chi2_vals, path):
+
+    vals = np.array([np.arange(1, iterations + 1), np.array(chi2_vals)])
+    np.save(path + 'chi2_vals.npy', vals)
+    logging.info('Saved chi 2 values at ({})'.format(path))
+
+
+def update_progress(progress):
+    barLength = 10  # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format(
+        "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+# 3
+
+############# LOGGER SETUP ###################
+
+
+LOG_DIRECTORY = datetime.now().strftime(
+    '/home/hmorenom/SSW_Files/OgModel/log_vel/log_%m_%d_%Y_%H_%M_%S/')
+
+os.mkdir(LOG_DIRECTORY)
+
+LOG_FILENAME = LOG_DIRECTORY + 'logfile.log'
+IMG_FILENAME = LOG_DIRECTORY + 'chi2_plot.png'
+
+logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(levelname)-8s %(message)s',
+                    level=logging.INFO, datefmt='%m-%d-%Y %H:%M:%S')
+
+logging.info('Start program')
+
+c1 = 4e-17
+c2 = 3
+c3 = 2e6
+c4 = 0.4
+c5 = 0.75
+c6 = 2.0e-15
+c7 = 30
+c8 = 0.8
+
+iterations = 300
+
+initial_params = [1.9702842924375007e-17, 4.848277359492187, 2000000.0, 0.4, 0.7129687499999999, 3.3015455000000006e-15, 22.97716875, 0.7867978284374999, 1800.0, 660.0, 750, 0.4]
+
+filenames = ['pred_c.save', 'pred_o.save', 'pred_fe.save']
+#filenames = ['pred_c.save', 'pred_n.save', 'pred_o.save', 'pred_ne.save','pred_mg.save','pred_si.save', 'pred_s.save', 'pred_fe.save']
+
+obs = get_observations()
+
+obs = [obs[0], obs[2], obs[-1]]  # isolate carbon for testing
+
+best_iteration, chi2_vals = run_MCMC(obs, initial_params, iterations, filenames, LOG_DIRECTORY, factor=0.05)
+
+logging.info('Finished iterating. Final parameters: {}. Final Chi^2 value: {}.'.format(best_iteration[0], np.sum(np.array(best_iteration[1]))))
+
+save_chi2(iterations, chi2_vals, LOG_DIRECTORY)
+
+plot_chi2(iterations, chi2_vals, IMG_FILENAME)
+
+logging.info('End of program.')
